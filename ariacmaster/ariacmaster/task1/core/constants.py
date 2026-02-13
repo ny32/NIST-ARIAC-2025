85e0b485481ecab0311c78684a9bf4f7345280ca
ny32 <awesomeny7@gmail.com>
2026-02-13 14:03:30 -0500

    updated default constants

--- ariacmaster/ariacmaster/task1/core/constants.py
+++ ariacmaster/ariacmaster/task1/core/constants.py
@@ -1,8 +1,8 @@
 NORMAL_CELL_VOLTAGE = 12.7 # Normal Voltage for a real cell
-ALLOWED_VOLTAGE_TOLERANCE = 7 # NIST ARIAC - Allowed voltage tolerance for a cell
+ALLOWED_VOLTAGE_TOLERANCE = 0.2 # NIST ARIAC - Allowed voltage tolerance for a cell
 
-INSPECTION_DEFECT_PROBABILITY = 1000 # Integers 1, 2, 3 represent defects, so values 3 or under will guaruntee defects
-# ex. if 10, 3/10 probability of defects 
+INSPECTION_DEFECT_PROBABILITY = 10 # Int: [1, 2, 3] represent defects
+# values 3 or less onlygauruntee defective cells to spawn 
 
 NORMAL_RETRIES=1000
 STARTUP_TIME = 0.1 # Time in seconds to simulate competition start
